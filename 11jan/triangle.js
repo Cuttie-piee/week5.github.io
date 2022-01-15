bbc4d509c248aef803bbb5438b5be6e7c209782a
Cuttie-piee <94856283+Cuttie-piee@users.noreply.github.com>
2022-01-15 22:10:55 +0530

    Delete triangle.js

--- 11jan/triangle.js
+++
@@ -1,9 +0,0 @@
-var n=parseInt(prompt("Enter the number "));
-var pattern=" ";
-for(var i=1;i<=n;i++){
-    for(var j=1;j<i+1;j++){
-       pattern += " * ";
-    }
-    pattern += " <br> ";
-}
-document.write(pattern);
